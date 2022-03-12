da9b710441e6e2f18c6d6c42583894d82316232c
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-12 23:42:11 +1030

    set up new landing page to match user story. added basic logic for rendering homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,17 +1,15 @@
+const { Post } = require('../models');
+
 module.exports = {
     getHome: (req, res) => {
         try {
+            // get login status
             const logged_in = req.session.logged_in;
-            res.status(200).render('dashboard', { logged_in });
-        } catch (err) {
-            //TODO: create error page
-            res.status(500).json(err);
-        }
-    },
 
-    getWelcome: (req, res) => {
-        try {
-            res.status(200).render('welcome');
+            // get all posts TODO this needs more arguments to included author etc
+            const posts = Post.findAll();
+
+            res.status(200).render('dashboard', { logged_in });
         } catch (err) {
             //TODO: create error page
             res.status(500).json(err);
