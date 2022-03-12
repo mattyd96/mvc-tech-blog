481791dfd34ca9048bbba9f2c361dcdc63759109
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-12 21:05:14 +1030

    added in error handling for signup that allows for a dynamic interface on the client side to show the end user where to change input

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 const { User, Post } = require('../models');
 
+const createErrorMessage = errors => {
+  const resError = {
+    errorList: []
+  }
+
+  errors.forEach(err => {
+    resError.errorList.push(err.path);
+  });
+
+  return resError;
+};
+
+
 module.exports = {
     login: (req, res) => {
 
@@ -8,40 +21,48 @@ module.exports = {
       console.log(req.body.username);
       try {
         // see if username already exists
-      const usernameExists = await User.findOne({   
-        where: { username: req.body.username }
-      });
-
-      // see if email already exists
-      const emailExists = await User.findOne({   
-        where: { email: req.body.email }
-      });
-
-      if (usernameExists) {
-        res.status(400).json({
-          message: 'username'
+        const usernameExists = await User.findOne({   
+          where: { username: req.body.username }
         });
-      }
 
-      if (emailExists) {
-        res.status(400).json({
-          message: 'email'
+        // if it does return error message
+        if (usernameExists) {
+          res.status(400).json({
+            error: ['username exists']
+          });
+        }
+
+
+        // see if email already exists
+        const emailExists = await User.findOne({   
+          where: { email: req.body.email }
         });
+
+        // if it does return error message
+        if (emailExists) {
+          res.status(400).json({
+            error: ['email exists'],
+          });
+        }
+
+        // Create New user
+        const newUser = await User.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        req.session.save(() => {
+          req.session.user_id = newUser.id;
+          req.session.logged_in = true;
+
+          res.redirect('/');
+        });
+  
+      } catch (err) {
+        const error = createErrorMessage(err.errors);
+        res.status(500).json(error);
       }
-
-      const newUser = User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      });
-
-      req.session.save(() => {
-        req.session.user_id = newUser.id;
-        req.session.logged_in = true;
-
-        res.redirect('/');
-      });
-      } catch (err) {console.log(err)}
       
     },
 };
