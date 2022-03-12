416c8364999fd450bfbc5dd313240c03721dba11
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-12 16:09:45 +1030

    added form for login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,13 @@
 module.exports = {
     getLoginForm: (req, res) => {
+
+        const loginForm = req.path === '/login' ? true : false;
+
         try {
-            res.status(200).render('loginForm');
+            loginForm ? 
+            res.status(200).render('loginForm') :
+            res.status(200).render('signupForm');
+
         } catch (err) {
             //TODO: create error page
             res.status(500).json(err);
