710a2fd3723a2bc447b4d83b5d52a17a79e57eae
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-12 12:16:13 +1030

    added auth middleware function

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./home-route.js');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
 
---
+++ helpers/auth.js
@@ -0,0 +1,5 @@
+const userAuth = (req, res, next) => {
+    req.session.loggedIn ? next() : res.redirect('welcome');
+};
+
+module.exports = userAuth;
