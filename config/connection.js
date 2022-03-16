3b72bb1d44f2d6ed615ff6a9bb4c358c975ff849
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-16 22:40:26 +1030

    set up for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
+const config = require('./db_heroku_config.json');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if(process.env.NODE_ENV) {
+  const sequelize = new Sequelize(process.env[config.heroku_db]);
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+
 
 module.exports = sequelize;
