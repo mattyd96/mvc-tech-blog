48125385e71fc6c7256fa38f0157ee4d1a91f58e
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-16 22:44:40 +1030

    error fix attempt for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize');
 const config = require('./db_heroku_config.json');
 require('dotenv').config();
 
+let sequelize;
+
 if(process.env.NODE_ENV) {
-  const sequelize = new Sequelize(process.env[config.heroku_db]);
+  sequelize = new Sequelize(process.env[config.heroku_db]);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
