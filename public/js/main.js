94a7b2ee5eab3f7e4aaf586fbb24bdbf6b8695fd
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-19 15:44:23 +1030

    added css styling to navbar and home page for mobile layouts

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,13 @@ const logout = async () => {
     console.log('error when logging out');
   }
 }
+
+const navToggle = () => {
+  const menu = document.querySelector('.menu-items');
+  const hamburger = document.querySelector('.hamburger-icon');
+  const close = document.querySelector('.close-icon');
+
+  menu.classList.toggle('mobile-hide');
+  hamburger.classList.toggle('mobile-hide');
+  close.classList.toggle('mobile-hide');
+};
